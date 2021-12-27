0375d5bf277686df01c6971a8af3bc1e69a89725
Daniel Reichhart <34721312+reichhartd@users.noreply.github.com>
2021-12-27 01:20:41 +0100

    Dependencies update (#3)

--- examples/jest-testing/App.js
+++ examples/jest-testing/App.js
@@ -14,8 +14,7 @@ const instructions = Platform.select({
     android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
