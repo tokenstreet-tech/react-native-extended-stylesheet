e175d5e797f824fdfe6e7b1d35de513cd2c852da
Daniel Reichhart <daniel.reichhart@gmx.net>
2021-12-29 15:10:55 +0100

    fix: testcase

--- src/__tests__/typeDefinition.test.ts
+++ src/__tests__/typeDefinition.test.ts
@@ -5,47 +5,51 @@ import { StyleSheet } from 'react-native';
 
 import EStyleSheet from '../';
 
-const eStyles = EStyleSheet.create({
-    $var: 10,
-    button1: {
-        width: () => '100%',
-        '@media (min-width: 350)': {
-            width: '$var',
-        },
-    },
-    button2: {
-        color: 'blue',
-        borderBottomWidth: EStyleSheet.hairlineWidth,
-    },
-    '@media ios': {
-        button2: {
-            width: '100%',
-        },
-    },
-});
+describe('typeDefinition', () => {
+    it('should not throw an error', () => {
+        const eStyles = EStyleSheet.create({
+            $var: 10,
+            button1: {
+                width: () => '100%',
+                '@media (min-width: 350)': {
+                    width: '$var',
+                },
+            },
+            button2: {
+                color: 'blue',
+                borderBottomWidth: EStyleSheet.hairlineWidth,
+            },
+            '@media ios': {
+                button2: {
+                    width: '100%',
+                },
+            },
+        });
 
-const styles = StyleSheet.create({
-    button1: {
-        ...EStyleSheet.absoluteFillObject,
-        color: 'red',
-    },
-    button2: {
-        color: 'blue',
-        borderBottomWidth: EStyleSheet.hairlineWidth,
-    },
-});
+        const styles = StyleSheet.create({
+            button1: {
+                ...EStyleSheet.absoluteFillObject,
+                color: 'red',
+            },
+            button2: {
+                color: 'blue',
+                borderBottomWidth: EStyleSheet.hairlineWidth,
+            },
+        });
 
-EStyleSheet.build();
-EStyleSheet.build({ $var: 'foo' });
-EStyleSheet.value('100%');
-EStyleSheet.value('100%', 'width');
-EStyleSheet.subscribe('build', () => {});
-EStyleSheet.clearCache();
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const x = EStyleSheet.absoluteFill;
-EStyleSheet.flatten([eStyles.button1, eStyles.button2]);
-EStyleSheet.flatten(styles.button1);
-EStyleSheet.flatten([styles.button1, styles.button2]);
-EStyleSheet.setStyleAttributePreprocessor('color', () => 'red');
+        EStyleSheet.build();
+        EStyleSheet.build({ $var: 'foo' });
+        EStyleSheet.value('100%');
+        EStyleSheet.value('100%', 'width');
+        EStyleSheet.subscribe('build', () => {});
+        EStyleSheet.clearCache();
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const x = EStyleSheet.absoluteFill;
+        EStyleSheet.flatten([eStyles.button1, eStyles.button2]);
+        EStyleSheet.flatten(styles.button1);
+        EStyleSheet.flatten([styles.button1, styles.button2]);
+        EStyleSheet.setStyleAttributePreprocessor('color', () => 'red');
+    });
+});
