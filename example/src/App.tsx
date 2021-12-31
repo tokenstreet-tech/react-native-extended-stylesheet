143116b1e3ab1323a3d89abd74eea280986b7dbe
Daniel Reichhart <daniel.reichhart@gmx.net>
2021-12-31 15:46:09 +0100

    fix: imports

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
+// @ts-expect-error Import remains to be able to switch between screens more easily
 import { HotModuleReloadScreen } from './screens/HotModuleReloadScreen';
+// @ts-expect-error Same description as above
 import { JestTestingScreen } from './screens/JestTestingScreen';
+// @ts-expect-error Same description as above
 import { MediaQueriesScreen } from './screens/MediaQueriesScreen';
+// @ts-expect-error Same description as above
 import { ReadmeScreen } from './screens/ReadmeScreen';
+// @ts-expect-error Same description as above
 import { RemScreen } from './screens/RemScreen';
 import { SimpleScreen } from './screens/SimpleScreen';
+// @ts-expect-error Same description as above
 import { ThemingScreen } from './screens/theming/ThemingScreen';
 /* eslint-enable @typescript-eslint/no-unused-vars */
 
