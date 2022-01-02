73751b0117674d42a795fe8ea23b6d3dda5341f9
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-01-02 16:59:08 +0100

    fix: borderBottomWidth

--- src/types/styles.ts
+++ src/types/styles.ts
@@ -4,25 +4,29 @@ import type { FlexStyle, ImageStyle, TextStyle, ViewStyle } from 'react-native';
 type TExtendedSizeValues = number | string | undefined;
 
 // Flex styles
-type TExtendedFlexStyle = {
-    [Key in keyof FlexStyle]: FlexStyle[Key] | (() => FlexStyle[Key]);
+type TOmittedFlexStyle = Omit<FlexStyle, keyof FlexStyle | 'borderBottomWidth'>;
+type TFuncFlexStyle = {
+    [Key in keyof TOmittedFlexStyle]: TOmittedFlexStyle[Key] | (() => TOmittedFlexStyle[Key]);
 };
+interface IExtendedFlexStyle extends TFuncFlexStyle {
+    borderBottomWidth?: TExtendedSizeValues;
+}
 
 // View styles
-type TOmittedViewStyle = Omit<ViewStyle, keyof FlexStyle | 'borderRadius'>;
+type TOmittedViewStyle = Omit<ViewStyle, keyof IExtendedFlexStyle | 'borderRadius'>;
 type TFuncViewStyle = {
     [Key in keyof TOmittedViewStyle]: TOmittedViewStyle[Key] | (() => TOmittedViewStyle[Key]);
 };
-interface IExtendedViewStyle extends TExtendedFlexStyle, TFuncViewStyle {
+interface IExtendedViewStyle extends IExtendedFlexStyle, TFuncViewStyle {
     borderRadius?: TExtendedSizeValues;
 }
 
 // Image styles
-type TOmittedImageStyle = Omit<ImageStyle, keyof FlexStyle>;
+type TOmittedImageStyle = Omit<ImageStyle, keyof IExtendedFlexStyle>;
 type TFuncImageStyle = {
     [Key in keyof TOmittedImageStyle]: TOmittedImageStyle[Key] | (() => TOmittedImageStyle[Key]);
 };
-interface IExtendedImageStyle extends TExtendedFlexStyle, TFuncImageStyle {}
+interface IExtendedImageStyle extends IExtendedFlexStyle, TFuncImageStyle {}
 
 // Text styles
 type TOmittedTextStyle = Omit<TextStyle, keyof ViewStyle | 'fontSize'>;
