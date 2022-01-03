df8d1a84bce2e4075affba7d520f05cb3ae961ac
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-01-03 23:14:53 +0100

    fix: supporting variable values

--- src/types/extendedStyles.ts
+++ src/types/extendedStyles.ts
@@ -13,41 +13,55 @@ type TMediaQueriesKeys = `@media ${string}`;
 // Flex styles
 type TFlexStyleSizeKeys = Pick<FlexStyle, 'borderBottomWidth'>;
 type TOmittedFlexStyle = Omit<FlexStyle, keyof TFlexStyleSizeKeys>;
-type TFuncFlexStyle = {
-    [Key in keyof TOmittedFlexStyle]: TOmittedFlexStyle[Key] | (() => TOmittedFlexStyle[Key]);
-};
 type TFlexStyleSize = Partial<Record<keyof TFlexStyleSizeKeys, TExtendedSizeValues>>;
-interface IExtendedFlexStyle extends TFlexStyleSize, TFuncFlexStyle {}
+type TCombinedFlexStyle = TFlexStyleSize & TOmittedFlexStyle;
+type TFuncFlexStyle = {
+    [Key in keyof TCombinedFlexStyle]:
+        | TCombinedFlexStyle[Key]
+        | TExtendedVariablesKeys
+        | (() => TCombinedFlexStyle[Key]);
+};
+interface IExtendedFlexStyle extends TFuncFlexStyle {}
 
 // View styles
 type TViewStyleSizeKeys = Pick<Omit<ViewStyle, keyof FlexStyle>, 'borderRadius'>;
 type TOmittedViewStyle = Omit<ViewStyle, keyof IExtendedFlexStyle | keyof TViewStyleSizeKeys>;
-type TFuncViewStyle = {
-    [Key in keyof TOmittedViewStyle]: TExtendedVariablesKeys | TOmittedViewStyle[Key] | (() => TOmittedViewStyle[Key]);
-};
 type TViewStyleSize = Partial<Record<keyof TViewStyleSizeKeys, TExtendedSizeValues>>;
-interface IExtendedViewStyle extends IExtendedFlexStyle, TFuncViewStyle, TViewStyleSize {}
+type TCombinedViewStyle = TOmittedViewStyle & TViewStyleSize;
+type TFuncViewStyle = {
+    [Key in keyof TCombinedViewStyle]:
+        | TCombinedViewStyle[Key]
+        | TExtendedVariablesKeys
+        | (() => TCombinedViewStyle[Key]);
+};
+
+interface IExtendedViewStyle extends IExtendedFlexStyle, TFuncViewStyle {}
 
 // Image styles
 type TImageStyleSizeKeys = Pick<Omit<ImageStyle, keyof FlexStyle>, 'borderRadius'>;
 type TOmittedImageStyle = Omit<ImageStyle, keyof IExtendedFlexStyle | keyof TImageStyleSizeKeys>;
-type TFuncImageStyle = {
-    [Key in keyof TOmittedImageStyle]:
-        | TExtendedVariablesKeys
-        | TOmittedImageStyle[Key]
-        | (() => TOmittedImageStyle[Key]);
-};
 type TImageStyleSize = Partial<Record<keyof TImageStyleSizeKeys, TExtendedSizeValues>>;
-interface IExtendedImageStyle extends IExtendedFlexStyle, TFuncImageStyle, TImageStyleSize {}
+type TCombinedImageStyle = TImageStyleSize & TOmittedImageStyle;
+type TFuncImageStyle = {
+    [Key in keyof TCombinedImageStyle]:
+        | TCombinedImageStyle[Key]
+        | TExtendedVariablesKeys
+        | (() => TCombinedImageStyle[Key]);
+};
+interface IExtendedImageStyle extends IExtendedFlexStyle, TFuncImageStyle {}
 
 // Text styles
 type TTextStyleSizeKeys = Pick<Omit<TextStyle, keyof ViewStyle>, 'fontSize'>;
 type TOmittedTextStyle = Omit<TextStyle, keyof TTextStyleSizeKeys | keyof ViewStyle>;
-type TFuncTextStyle = {
-    [Key in keyof TOmittedTextStyle]: TExtendedVariablesKeys | TOmittedTextStyle[Key] | (() => TOmittedTextStyle[Key]);
-};
 type TTextStyleSize = Partial<Record<keyof TTextStyleSizeKeys, TExtendedSizeValues>>;
-interface IExtendedTextStyle extends IExtendedViewStyle, TFuncTextStyle, TTextStyleSize {}
+type TCombinedTextStyle = TOmittedTextStyle & TTextStyleSize;
+type TFuncTextStyle = {
+    [Key in keyof TCombinedTextStyle]:
+        | TCombinedTextStyle[Key]
+        | TExtendedVariablesKeys
+        | (() => TCombinedTextStyle[Key]);
+};
+interface IExtendedTextStyle extends IExtendedViewStyle, TFuncTextStyle {}
 
 // Export
 type TExtendedStyles = IExtendedImageStyle | IExtendedTextStyle | IExtendedViewStyle;
