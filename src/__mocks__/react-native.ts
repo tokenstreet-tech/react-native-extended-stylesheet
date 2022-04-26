e6c23d5e84ca761ab08aea336e8f4180d4f55d82
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-04-26 11:22:19 +0200

    fix: test

--- src/__mocks__/react-native.ts
+++ src/__mocks__/react-native.ts
@@ -2,16 +2,9 @@ import type ReactNative from 'react-native';
 import type { DeepPartial } from 'utility-types';
 
 const reactNativeMock: DeepPartial<typeof ReactNative> = {
-    Dimensions: {
-        get: () => ({ width: 100, height: 200, scale: 1, fontScale: 1 }),
-    },
-    Platform: {
-        OS: 'ios',
-        select: jest.fn(),
-    },
-    I18nManager: {
-        isRTL: false,
-    },
+    Dimensions: { get: () => ({ width: 100, height: 200, scale: 1, fontScale: 1 }) },
+    Platform: { OS: 'ios', select: jest.fn() },
+    I18nManager: { isRTL: false },
     StyleSheet: {
         create: (obj) =>
             Object.keys(obj).reduce((res: any, key, index) => {
@@ -21,6 +14,8 @@ const reactNativeMock: DeepPartial<typeof ReactNative> = {
         flatten: (arr: any) => arr.reduce((res: any, item: any) => Object.assign(res, item), {}),
         hairlineWidth: 1,
     },
+    View: jest.fn().mockReturnValue('View'),
+    Text: jest.fn().mockReturnValue('Text'),
 };
 
 module.exports = reactNativeMock;
