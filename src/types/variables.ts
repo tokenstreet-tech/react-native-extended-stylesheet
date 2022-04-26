b227c43f484dafdbca297ef437e8ad5c11fa72a8
Daniel Reichhart <daniel.reichhart@gmx.net>
2022-04-26 12:20:46 +0200

    fix: types

--- src/types/variables.ts
+++ src/types/variables.ts
@@ -1,11 +1,15 @@
-import type { TExtendedStyles, TStyles } from './extendedStyles';
+import type { TExtendedStyles } from './extendedStyles';
 
 // Extended
 export type TExtendedVariablesKeys = `$${string}`;
-// TODO: Improve type with generics. Because sometimes you need exactly a number or a string, and not a union type.
+
 type TExtendedVariablesReturnValues = TExtendedStyles | number | string;
 export type TExtendedVariablesValues = TExtendedVariablesReturnValues | (() => TExtendedVariablesReturnValues);
 
 // Standard
 export type TVariablesKeys = TExtendedVariablesKeys;
-export type TVariablesValues = TStyles | number | string;
+/*
+ * TODO: Improve type with generics. Because sometimes you need exactly a number or a string, and not a union type.
+ * export type TVariablesValues = TStyles | number | string;
+ */
+export type TVariablesValues = any;
