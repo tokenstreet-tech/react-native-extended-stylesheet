af6621f5b13cf4e1d84f566f7e79206e8124ada7
Daniel Reichhart <daniel.reichhart@gmx.net>
2023-02-26 17:18:01 +0100

    fix: draft

--- packages/website/src/components/HomepageFeatures/HomepageFeatures.tsx
+++ packages/website/src/components/HomepageFeatures/HomepageFeatures.tsx
@@ -1,51 +1,59 @@
+import { useColorMode } from '@docusaurus/theme-common';
 import type { IFeatureItem } from '@site/src/components/HomepageFeatures/FeatureItem';
 import { FeatureItem } from '@site/src/components/HomepageFeatures/FeatureItem';
+import dependabot from '@site/static/img/dependabot.svg';
+import maintenance from '@site/static/img/maintenance.svg';
+import maintenanceDark from '@site/static/img/maintenanceDark.svg';
+import typescript from '@site/static/img/typescript.svg';
 import type { FC } from 'react';
 import React from 'react';
 
 import styles from './HomepageFeatures.module.css';
 
-const FeatureList: Array<IFeatureItem> = [
-    {
-        title: 'Easy to Use',
-        Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-        description: (
-            <>
-                Docusaurus was designed from the ground up to be easily installed and used to get your website up and
-                running quickly.
-            </>
-        ),
-    },
-    {
-        title: 'Focus on What Matters',
-        Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-        description: (
-            <>
-                Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your docs into
-                the <code>docs</code> directory.
-            </>
-        ),
-    },
-    {
-        title: 'Powered by React',
-        Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-        description: (
-            <>
-                Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the
-                same header and footer.
-            </>
-        ),
-    },
-];
+export const HomepageFeatures: FC = () => {
+    const { isDarkTheme } = useColorMode();
 
-export const HomepageFeatures: FC = () => (
-    <section className={styles.features}>
-        <div className="container">
-            <div className="row">
-                {FeatureList.map((props, idx) => (
-                    <FeatureItem key={idx} {...props} />
-                ))}
+    const FeatureList: Array<IFeatureItem> = [
+        {
+            Svg: typescript,
+            description: (
+                <>
+                    Docusaurus was designed from the ground up to be easily installed and used to get your website up
+                    and running quickly.
+                </>
+            ),
+            title: 'IntelliSense IDE integration',
+        },
+        {
+            Svg: isDarkTheme ? maintenanceDark : maintenance,
+            description: (
+                <>
+                    Latest dependencies of the IDnow <a href="https://github.com/idnow/de.idnow.ios">iOS</a> and{' '}
+                    <a href="https://github.com/idnow/de.idnow.android">Android</a> SDKs. Secured by Dependabot.
+                </>
+            ),
+            title: 'Always up to date',
+        },
+        {
+            Svg: dependabot,
+            description: (
+                <>
+                    Strong type declarations are included in this package. There isn&apos;t a single <code>any</code>.
+                </>
+            ),
+            title: 'First class TypeScript support',
+        },
+    ];
+
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                <div className="row">
+                    {FeatureList.map((props, idx) => (
+                        <FeatureItem key={idx} {...props} />
+                    ))}
+                </div>
             </div>
-        </div>
-    </section>
-);
+        </section>
+    );
+};
