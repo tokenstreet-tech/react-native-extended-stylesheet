0b21bfc8f2a04e4e01157b2c8d385ba6465010bd
Daniel Reichhart <34721312+reichhartd@users.noreply.github.com>
2023-02-26 14:36:42 +0100

    fix: Website (#486)
    * fix: website
    
    * fix: website

--- packages/website/docusaurus.config.js
+++ packages/website/docusaurus.config.js
@@ -8,17 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'My Site',
+    title: 'React Native Extended StyleSheet',
     tagline: 'Dinosaurs are cool',
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-test-site.com',
+    url: 'https://tokenstreet-tech.github.io',
     /*
      * Set the /<baseUrl>/ pathname under which your site is served
      * For GitHub pages deployment, it is often '/<projectName>/'
      */
-    baseUrl: '/',
+    baseUrl: '/react-native-extended-stylesheet/',
 
     /*
      * GitHub pages deployment config.
