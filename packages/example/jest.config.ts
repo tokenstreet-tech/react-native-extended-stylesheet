d3100721bb608ea50cdce29545da6e88972779aa
Daniel Reichhart <34721312+reichhartd@users.noreply.github.com>
2023-02-26 14:27:29 +0100

    feat: Bootstrap website (#483)
    * feat: draft
    
    * feat: draft
    
    * fix: depl website
    
    * fix: depl website
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * chore: dependabot
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft
    
    * feat: draft

--- packages/example/jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from '@jest/types';
-
-const config: Config.InitialOptions = {
-    preset: 'react-native',
-    reporters: ['default', 'github-actions'],
-};
-
-export default config;
---
+++ packages/website/.lintstagedrc.js
@@ -0,0 +1,1 @@
+module.exports = require('@tokenstreet/react-native-extended-stylesheet-common/lintstagedrc.base.js');
