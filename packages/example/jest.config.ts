8838806d68ec1f0a4dd2f88f0886d4125862937b
Daniel Reichhart <daniel.reichhart@gmx.net>
2023-02-26 11:32:02 +0100

    feat: draft

--- packages/example/jest.config.ts
+++ packages/example/jest.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-    modulePathIgnorePatterns: ['<rootDir>/lib/'],
     preset: 'react-native',
     reporters: ['default', 'github-actions'],
 };
