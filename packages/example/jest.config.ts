0f558cfd9cc5bdb18b5931cd7b11e9166320fdb5
Daniel Reichhart <daniel.reichhart@gmx.net>
2023-02-26 13:43:59 +0100

    feat: draft

--- packages/example/jest.config.ts
+++ packages/example/jest.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-    preset: 'react-native',
+    preset: 'jest-expo',
     reporters: ['default', 'github-actions'],
 };
 
