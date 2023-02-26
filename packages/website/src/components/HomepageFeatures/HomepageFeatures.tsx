bed40b454c32a7946fd0a5d25a2520dd07a2b6d9
Daniel Reichhart <daniel.reichhart@gmx.net>
2023-02-26 17:35:35 +0100

    fix: draft

--- packages/website/src/components/HomepageFeatures/HomepageFeatures.tsx
+++ packages/website/src/components/HomepageFeatures/HomepageFeatures.tsx
@@ -6,42 +6,41 @@ import maintenance from '@site/static/img/maintenance.svg';
 import maintenanceDark from '@site/static/img/maintenanceDark.svg';
 import typescript from '@site/static/img/typescript.svg';
 import type { FC } from 'react';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import styles from './HomepageFeatures.module.css';
 
 export const HomepageFeatures: FC = () => {
     const { isDarkTheme } = useColorMode();
 
-    const FeatureList: Array<IFeatureItem> = [
-        {
-            Svg: typescript,
-            description: (
-                <>
+    const FeatureList = useMemo<Array<IFeatureItem>>(
+        () => [
+            {
+                Svg: typescript,
+                description: (
                     <a href="https://github.com/vitalets/react-native-extended-stylesheet/issues/118">
                         CSS intellisense not working #118
                     </a>
-                </>
-            ),
-            title: 'IntelliSense IDE integration',
-        },
-        {
-            Svg: isDarkTheme ? maintenanceDark : maintenance,
-            description: (
-                <>
+                ),
+                title: 'IntelliSense IDE integration',
+            },
+            {
+                Svg: isDarkTheme ? maintenanceDark : maintenance,
+                description: (
                     <a href="https://github.com/vitalets/react-native-extended-stylesheet/issues/154">
                         Is this repo active? #154
                     </a>
-                </>
-            ),
-            title: 'Maintained',
-        },
-        {
-            Svg: dependabot,
-            description: <>Latest dependencies - Secured by Dependabot</>,
-            title: 'Always up to date',
-        },
-    ];
+                ),
+                title: 'Maintained',
+            },
+            {
+                Svg: dependabot,
+                description: <>Latest dependencies - Secured by Dependabot</>,
+                title: 'Always up to date',
+            },
+        ],
+        [isDarkTheme]
+    );
 
     return (
         <section className={styles.features}>
